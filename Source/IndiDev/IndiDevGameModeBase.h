6bbebc4ad7be1b62b0ffc763654a1cda98300fb7
mike-sb <on_desk@mail.ru>
2024-03-16 23:13:47 +0300

    f

---
+++ Source/IndiDev/IndiDevGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IndiDevGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INDIDEV_API AIndiDevGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
